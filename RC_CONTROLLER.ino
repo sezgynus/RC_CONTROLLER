043da55c58a0acbc6fbcc0509d3492342b0f4fdf
Sezgin AÇIKGÖZ <sezginacikgoz@mail.com>
2026-01-21 14:08:14 +0300

    demo app
    Signed-off-by: Sezgin AÇIKGÖZ <sezginacikgoz@mail.com>

--- RC_CONTROLLER.ino
+++ RC_CONTROLLER.ino
@@ -1,9 +1,14 @@
+#include "RcCarController.h"
+#include "RcHardwareDriver.h"
+RcCarController controller;
+RcHardwareDriver hardware;
 void setup() {
   // put your setup code here, to run once:
-
+  hardware.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  controller.update(millis());
+  hardware.update(controller);
 }
